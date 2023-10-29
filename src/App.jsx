16480c74bc921c552c98084a3e24b31899112e6f
Rusul10 <rusul10amer@gmail.com>
2023-10-29 20:14:34 +0300

    book table

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Main from './components/Main'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <Nav/>
+      <Main/>
+      <Menu/>
+      <Footer/>
+    </div>
+  )
+}
